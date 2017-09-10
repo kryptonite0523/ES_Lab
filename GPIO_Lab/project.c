affeafb6dc9ec0094943709c65289d7b86a41b34
kryptonite0523 <31293496+kryptonite0523@users.noreply.github.com>
2017-09-10 15:49:03 -0400

    ch.3
    Change #3

--- GPIO_Lab/project.c
+++ GPIO_Lab/project.c
@@ -16,7 +16,7 @@ int  main(void)
 {
     volatile uint32_t ui32Loop;
 	
-    // Enable the GPIO port that is used for the on-board LED.
+    // Enable the GPIO port used for the on-board LED.
 		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     
 		// Check if the peripheral access is enabled.
@@ -37,15 +37,16 @@ int  main(void)
 		GPIOPinTypeGPIOInput (GPIO_PORTF_BASE, GPIO_PIN_4);
 		
 		
-		
-		while(1)
+		//While switch 0 is being held down do this skim through the colors...(atleast that
+		// is the plan.
+		while(GPIO_PIN_0 == 0xF)
     {
-				
+				//BLINK RED LED
         // Turn on the LED.
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0xF);
 
         // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+        for(ui32Loop = 0; ui32Loop < 800000; ui32Loop++)
         {
         }
 
@@ -53,7 +54,41 @@ int  main(void)
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x0);
 
         // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+        for(ui32Loop = 0; ui32Loop < 80000; ui32Loop++)
+        {
+        }
+				
+				//BLINK BLUE LED
+				// Turn on the LED.
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0xF);
+
+        // Delay for a bit.
+        for(ui32Loop = 0; ui32Loop < 800000; ui32Loop++)
+        {
+        }
+
+        // Turn off the LED.
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0x0);
+
+        // Delay for a bit.
+        for(ui32Loop = 0; ui32Loop < 80000; ui32Loop++)
+        {
+        }
+				
+					//BLINK GREEN LED
+				// Turn on the LED.
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0xF);
+
+        // Delay for a bit.
+        for(ui32Loop = 0; ui32Loop < 800000; ui32Loop++)
+        {
+        }
+
+        // Turn off the LED.
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0x0);
+
+        // Delay for a bit.
+        for(ui32Loop = 0; ui32Loop < 80000; ui32Loop++)
         {
         }
     }
