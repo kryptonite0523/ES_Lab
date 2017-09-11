6273e294cf92fbb6352cfbe403a460ebfe3ae90e
astorm22 <31227112+astorm22@users.noreply.github.com>
2017-09-11 14:12:12 -0400

    Reconfigured Code For Use On EK-TM4C1294XL
    The code runs however LED #3 is not illuminating under any circumstances..

--- GPIO_Lab/project.c
+++ GPIO_Lab/project.c
@@ -11,14 +11,16 @@ __error__(char *pcFilename, uint32_t ui32Line)
 }
 #endif
 
-
 int  main(void)
 {
     volatile uint32_t ui32Loop;
+		int	delay1=600000;
 	
     // Enable the GPIO port used for the on-board LED.
 		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ); 	
+	
 		// Check if the peripheral access is enabled.
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
     {
@@ -28,68 +30,58 @@ int  main(void)
 		
 		// Enable the GPIO pin for the LED (PF1,PF2,PF3).  Set the direction as output, and
     // enable the GPIO pin for digital function.
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
-		GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
-		GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1);
+		GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+		GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_4);
+		GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0);
 	
-    //Enable the GPIO Pin for the switches (PF0,PF4). Set the direction as an input?
-		GPIOPinTypeGPIOInput (GPIO_PORTF_BASE, GPIO_PIN_0);
-		GPIOPinTypeGPIOInput (GPIO_PORTF_BASE, GPIO_PIN_4);
+    //Enable the GPIO Pin for the switches (PJ0,PJ1). Set the direction as an input?
+		GPIOPinTypeGPIOInput (GPIO_PORTJ_BASE, GPIO_PIN_0);
+		GPIOPinTypeGPIOInput (GPIO_PORTJ_BASE, GPIO_PIN_1);
 		
 		
-		//While switch 0 is being held down do this skim through the colors...(atleast that
-		// is the plan.
-		while(GPIO_PIN_0 == 0xF)
-    {
-				//BLINK RED LED
-        // Turn on the LED.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0xF);
-
-        // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 800000; ui32Loop++)
-        {
-        }
-
-        // Turn off the LED.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x0);
-
-        // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 80000; ui32Loop++)
-        {
-        }
-				
-				//BLINK BLUE LED
-				// Turn on the LED.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0xF);
-
-        // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 800000; ui32Loop++)
-        {
-        }
-
-        // Turn off the LED.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0x0);
-
-        // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 80000; ui32Loop++)
-        {
-        }
-				
-					//BLINK GREEN LED
-				// Turn on the LED.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0xF);
-
-        // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 800000; ui32Loop++)
-        {
-        }
-
-        // Turn off the LED.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0x0);
-
-        // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 80000; ui32Loop++)
-        {
-        }
-    }
-}
+		//While switch 1 is being held down the first LED will illuminate
+		//While switch 2 is being held down the second LED will illuminate
+		//If neither condition is met the fourth LED will illuminate
+		while(1)
+		{
+			GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0xF);
+		
+      SysCtlDelay(delay1);		
+		
+			GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0x0);
+			
+			GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0xF);
+		
+      SysCtlDelay(delay1);		
+		
+			GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0x0);
+			
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0xF);
+		
+			SysCtlDelay(delay1);
+			
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x0);
+		
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0xF);
+			
+			SysCtlDelay(delay1);
+			
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0x0);
+			
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, 0xF);
+			
+			SysCtlDelay(delay1);
+			
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, 0x0);
+			
+			GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0xF);
+			
+			SysCtlDelay(delay1);
+			
+			GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0x0);
+			
+			SysCtlDelay(delay1);
+		}
+	
+	}
